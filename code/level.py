720d8136096550287ac84299245448e7411b6437
GreenCrimea <tom.horton100@gmail.com>
2022-07-06 23:37:31 +1000

    .

--- code/level.py
+++ code/level.py
@@ -14,11 +14,11 @@ class Level:
     
     def create_map(self):
         for row_index,row in enumerate(WORLD_MAP):
-            for col_index,col in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
                     Tile((x,y),[self.visible_sprites])
             
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
