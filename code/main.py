fe6cee49027c62044a581f3c9e599fd15ccb7caf
GreenCrimea <tom.horton100@gmail.com>
2022-07-06 23:10:04 +1000

    .

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from settings import *
 from debug import debug
+from level import Level
 
 class Game:
     def __init__(self):
@@ -10,6 +11,8 @@ class Game:
         pygame.display.set_caption('GAME_TEST')
         self.clock = pygame.time.Clock()
         
+        self.level = Level()
+        
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -18,6 +21,7 @@ class Game:
                     sys.exit()
             
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
