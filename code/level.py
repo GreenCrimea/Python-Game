66fd3b26c6a28a07314ca059c9b77c157d5bcaf5
GreenCrimea <tom.horton100@gmail.com>
2022-07-07 05:32:31 +1000

    .

--- code/level.py
+++ code/level.py
@@ -3,6 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
 
 
 class Level:
@@ -15,14 +16,20 @@ class Level:
         self.create_map()
     
     def create_map(self):
-        for row_index,row in enumerate(WORLD_MAP):
-            for col_index,col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
-                if col == 'p':
-                    self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
+        layouts = {
+                'boundary': import_csv_layout('./tmp/map/map_FloorBlocks.csv')
+        }
+        
+        for style,layout in layouts.items():
+            for row_index,row in enumerate(layout):
+                for col_index,col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y),[self.visible_sprites, self.obstacles_sprites], 'invisible') 
+        
+        self.player = Player((2000,1430),[self.visible_sprites],self.obstacles_sprites)
             
     def run(self):
         self.visible_sprites.custom_draw(self.player )
@@ -38,10 +45,16 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
         
+        self.floor_surface = pygame.image.load('./tmp/graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0,0))
+        
     def custom_draw(self,player):
         
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+        
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface,floor_offset_pos)
         
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
