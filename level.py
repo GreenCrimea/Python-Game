83c09d397fe3783bcbc20e8ed7e9d80eb8854973
greencrimea <tom.horton100@gmail.com>
2022-08-18 21:20:06 +1000

    .

--- level.py
+++ level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, WildFlower, Trees
+from sprites import Generic, Water, WildFlower, Trees, Interaction
 from pytmx.util_pygame import load_pygame
 from support import *
 
@@ -18,6 +18,7 @@ class Level:
         self.all_sprites = CameraGroup()
         self.collision_sprites = pygame.sprite.Group()
         self.tree_sprites = pygame.sprite.Group()
+        self.interaction_sprites = pygame.sprite.Group()
 
         self.setup()
         self.overlay = Overlay(self.player)
@@ -58,7 +59,9 @@ class Level:
         #player
         for obj in tmx_data.get_layer_by_name('Player'):
             if obj.name == "Start":
-                self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites, self.tree_sprites)
+                self.player = Player((obj.x,obj.y), self.all_sprites, self.collision_sprites, self.tree_sprites, self.interaction_sprites)
+            if obj.name == 'Bed':
+                Interaction((obj.x,obj.y), (obj.width,obj.height), self.interaction_sprites, obj.name)
         
         Generic(    pos = (0,0), 
                     surf = pygame.image.load('../Python-Game/graphics/world/ground.png').convert_alpha(), 
